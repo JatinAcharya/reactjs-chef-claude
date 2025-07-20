00661503b5f2090ec9e7da84d57ed54bb51c8229
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-07-20 18:21:06 +0530

    input for ingredients configured

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 
 const Main = () => {
+  // const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const addIngredient = (formData) => {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  };
+
+  const ingredientsListItems = ingredients.map((ingredient) => {
+    return <li>{ingredient}</li>;
+  });
+
   return (
     <>
       <main>
-        <form className="add-ingredient-form">
+        <form action={addIngredient} className="add-ingredient-form">
           <input
             aria-label="Add ingredient"
             type="text"
             placeholder="e.g. oregano"
+            name="ingredient"
           />
           <button>Add Ingredient</button>
         </form>
+        <ul>{ingredientsListItems}</ul>
       </main>
     </>
   );
